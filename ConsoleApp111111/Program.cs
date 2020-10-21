81d231416156fcb300a2dd9572baa7370497ab44
Gáspár <gaspar.szabolcs@vereb.dc>
2020-10-21 11:10:44 +0200

    Add project files.

---
+++ ConsoleApp111111/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp111111
+{
+    class Program
+    {
+
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            var t = new int[10];
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = r.Next(10, 100);
+                Console.Write($"{t[i]}, ");
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
